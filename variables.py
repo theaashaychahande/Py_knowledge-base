580274f0c1da01aae0b2b53497ea2cd4822ff8e9
Aashay Chahande <aashaychahande1717@gmail.com>
2025-07-21 00:20:01 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,22 @@
+"""
+1. Definition:  
+Variables are like labeled boxes where you store data (numbers, text, etc.)
+2. Why Use:  
+To remember values for later and make code flexible
+"""
+
+# 3) Format :
+# variable_name = value  
+
+# 4) Code Examples:  
+name = "Alex"       # String (text)  
+age = 19            # Integer (whole number)  
+height = 5.11       # Float (decimal)  
+is_student = True   # Boolean (True/False)  
+
+print(name)  
+print(age + 5)  
+
+# 5) Output:  
+# Alex  
+# 24  
