8015e7a5b80a1e94289b9d4028ee4ce7b1e95d66
Aashay Chahande <aashaychahande1717@gmail.com>
2025-07-24 01:49:41 +0530

    Created scientific_computing.py

---
+++ Advanced Level/scientific_computing.py
@@ -0,0 +1,18 @@
+"""
+1. Definition:  
+Numerical computing with NumPy
+2. Why Use:  
+Fast math operations on arrays
+"""
+
+
+import numpy as np
+
+grades = np.array([85, 92, 78])
+print(f"Average: {np.mean(grades)}")
+
+
+"""
+Output:
+Average: 85.0
+"""
