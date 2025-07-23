ae728290692bc8a55df931532376a60c0ecbf33c
Aashay Chahande <aashaychahande1717@gmail.com>
2025-07-24 01:52:10 +0530

    Created automation.py

---
+++ Advanced Level/automation.py
@@ -0,0 +1,17 @@
+"""
+1. Definition:  
+Scripting repetitive tasks 
+2. Why Use:  
+Save time on manual work 
+"""
+
+
+import os
+
+for i in range(3):
+    os.mkdir(f"project_{i+1}")
+  
+
+"""
+(Creates folders project_1, project_2, project_3)
+"""
