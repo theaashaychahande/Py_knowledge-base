85e008c2e3f4da3226a479d2a545f449c36a89a3
Aashay Chahande <aashaychahande1717@gmail.com>
2025-07-24 01:47:54 +0530

    graphical user interface

---
+++ Advanced Level/gui_basics.py
@@ -0,0 +1,20 @@
+"""
+1. Definition:  
+Creating graphical interfaces
+2. Why Use:  
+Build user-friendly apps
+"""
+
+
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Priyanka's App")
+
+label = tk.Label(text="Hello Tkinter!")
+label.pack()
+
+
+"""
+(Shows small window with label)
+"""
